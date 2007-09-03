453d352896389c69cfac46c42953ff5a2cb0c7f2
Alexander Rodin <rodin.alexander@gmail.com>
2007-09-03 13:20:58 +0000

    Tagging the 0.07 release of QStarDict

--- src/resizablepopup.h
+++ src/resizablepopup.h
@@ -1,20 +1,21 @@
-/********************************************************************************
- * resizablepopup.h - QStarDict, a StarDict clone written with using Qt         *
- * Copyright (C) 2007 Alexander Rodin                                           *
- *                                                                              *
- * This program is free software: you can redistribute it and/or modify         *
- * it under the terms of the GNU General Public License as published by         *
- * the Free Software Foundation, either version 3 of the License, or            *
- * (at your option) any later version.                                          *
- *                                                                              *
- * This program is distributed in the hope that it will be useful,              *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of               *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                *
- * GNU General Public License for more details.                                 *
- *                                                                              *
- * You should have received a copy of the GNU General Public License            *
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.        *
- ********************************************************************************/
+/*****************************************************************************
+ * resizablepopup.h - QStarDict, a StarDict clone written with using Qt      *
+ * Copyright (C) 2007 Alexander Rodin                                        *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or modify      *
+ * it under the terms of the GNU General Public License as published by      *
+ * the Free Software Foundation; either version 2 of the License, or         *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This program is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ * GNU General Public License for more details.                              *
+ *                                                                           *
+ * You should have received a copy of the GNU General Public License along   *
+ * with this program; if not, write to the Free Software Foundation, Inc.,   *
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.               *
+ *****************************************************************************/
 
 #ifndef RESIZABLEPOPUP_H
 #define RESIZABLEPOPUP_H
@@ -76,6 +77,3 @@ class ResizablePopup: public QFrame
 };
 
 #endif // RESIZABLEPOPUP_H
-
-// vim: tabstop=4 softtabstop=4 shiftwidth=4 expandtab cindent textwidth=120 formatoptions=tc
-
--- src/settingsdialog.h
+++ src/settingsdialog.h
@@ -1,20 +1,21 @@
-/********************************************************************************
- * settingsdialog.h - QStarDict, a StarDict clone written with using Qt         *
- * Copyright (C) 2007 Alexander Rodin                                           *
- *                                                                              *
- * This program is free software: you can redistribute it and/or modify         *
- * it under the terms of the GNU General Public License as published by         *
- * the Free Software Foundation, either version 3 of the License, or            *
- * (at your option) any later version.                                          *
- *                                                                              *
- * This program is distributed in the hope that it will be useful,              *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of               *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                *
- * GNU General Public License for more details.                                 *
- *                                                                              *
- * You should have received a copy of the GNU General Public License            *
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.        *
- ********************************************************************************/
+/*****************************************************************************
+ * settingsdialog.h - QStarDict, a StarDict clone written with using Qt      *
+ * Copyright (C) 2007 Alexander Rodin                                        *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or modify      *
+ * it under the terms of the GNU General Public License as published by      *
+ * the Free Software Foundation; either version 2 of the License, or         *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This program is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ * GNU General Public License for more details.                              *
+ *                                                                           *
+ * You should have received a copy of the GNU General Public License along   *
+ * with this program; if not, write to the Free Software Foundation, Inc.,   *
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.               *
+ *****************************************************************************/
 
 #ifndef SETTINGSDIALOG_H
 #define SETTINGSDIALOG_H
@@ -51,6 +52,3 @@ class SettingsDialog: public QDialog, private Ui::SettingsDialog
 };
 
 #endif // SETTINGSDIALOG_H
-
-// vim: tabstop=4 softtabstop=4 shiftwidth=4 expandtab cindent textwidth=120 formatoptions=tc
-
