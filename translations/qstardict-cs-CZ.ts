1eaf02b5e0c488cb4e73c09e7af0aa0888562ec9
Alexander Rodin <rodin.alexander@gmail.com>
2007-09-19 18:08:16 +0000

    Add czech translation

---
+++ translations/qstardict-cs-CZ.ts
@@ -0,0 +1,323 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="cs_CZ">
+<context>
+    <name>DictCore</name>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Not found!</source>
+        <translation>Nenalezeno!</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>QStarDict</source>
+        <translation>QStarDict</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>About QStarDict</source>
+        <translation>O aplikaci QStarDict</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>&lt;b&gt;QStarDict </source>
+        <translation>&lt;b&gt;QStarDict </translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>&lt;/b&gt; - Qt version of StarDict&lt;br&gt;</source>
+        <translation>&lt;/b&gt; - Qt verze StarDict&lt;br&gt;</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Copyright (C) 2007 Alexander Rodin &lt;a href=&quot;http://qstardict.ylsoftware.com&quot;&gt;http://qstardict.ylsoftware.com&lt;/a&gt;</source>
+        <translation>Copyright (C) 2007Alexander Rodin &lt;a href=&quot;http://qstardict.ylsoftware.com&quot;&gt;http://qstardict.ylsoftware.com&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Save translation</source>
+        <translation>Uložit překlad</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Error</source>
+        <translation>Chyba</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Cannot save translation</source>
+        <translation>Není možné uložit překlad</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Clear the search box</source>
+        <translation>Vymazat pole pro vyhledávání</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Clear</source>
+        <translation>Vymazat</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Fuzzy query</source>
+        <translation>Fuzzy query</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Search</source>
+        <translation>Vyhledat</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Words list</source>
+        <translation>Seznam slov</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>&amp;Settings</source>
+        <translation>&amp;Nastavení</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>&amp;File</source>
+        <translation>&amp;Soubor</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Konec</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Ctrl+Q</source>
+        <translation>Ctrl+Q</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>&amp;About</source>
+        <translation>&amp;О aplikaci QStarDict</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>About &amp;Qt</source>
+        <translation>О &amp;verzi Qt</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>&amp;Configure QStarDict</source>
+        <translation>&amp;Nastavení QStarDict</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>&amp;Scan</source>
+        <translation>&amp;Vyhledávat</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>&amp;Save to file</source>
+        <translation>&amp;Uložit do souboru</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Ctrl+S</source>
+        <translation>Ctrl+s</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Nápověda</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>%1 - QStarDict</source>
+        <translation>%1 - QStarDict</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Select dictionaries directory</source>
+        <translation>Vybrat adresáře se slovníky</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Dictionaries</source>
+        <translation>Slovníky</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Order</source>
+        <translation>Pořadí</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Disabled dictionaries:</source>
+        <translation>Nepoužívané slovníky:</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Left</source>
+        <translation>Vlevo</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Down</source>
+        <translation>Dolu</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Up</source>
+        <translation>Nahoru</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Right</source>
+        <translation>Vpravo</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Ordered dictionaries:</source>
+        <translation>Používané slovníky:</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Directories</source>
+        <translation>Adresáře</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Add</source>
+        <translation>Přidat</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Remove</source>
+        <translation>Odstranit</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Popup window</source>
+        <translation>Plovoucí okno</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Scan selection</source>
+        <translation>Vyhledat výběr</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Show only if modifier pressed</source>
+        <translation>Zobrazit pouze při stistku klávesy</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Alt</source>
+        <translation>Alt</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Control</source>
+        <translation>Control</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Shift</source>
+        <translation>Shift</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Win</source>
+        <translation>Win</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>%</source>
+        <translation>%</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Show if word not found</source>
+        <translation>Zobrazit pokud se slovo nenajde</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>QStarDict Settings</source>
+        <translation>Nastavení QStarDict</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Apperance</source>
+        <translation>Vzhled</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Opacity</source>
+        <translation>Sytost</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Default width</source>
+        <translation>Výchozí šířka</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Default height</source>
+        <translation>Výchozí výška</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Behavior</source>
+        <translation>Chování</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Timeout before hide after mouse over</source>
+        <translation>Za jakou dobu se zavře okno s překladem
+potom co kurzor opustí výběr</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source> sec</source>
+        <translation>sek</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Translation view</source>
+        <translation>Zobrazení překladu</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Expand abbreviations</source>
+        <translation>Rozepisovat zkratky</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Reformat translations</source>
+        <translation>Přeformátovávat překlad</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>&lt;b&gt;&lt;font color=&quot;red&quot;&gt;Warning!&lt;/font&gt;&lt;/b&gt; Reformated translations may be not correct displayed</source>
+        <translation>&lt;b&gt;&lt;font color=&quot;red&quot;&gt;Varování!&lt;/font&gt;&lt;/b&gt; Přeformátované překlady se nemusí správně zobrazit</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Don&apos;t hide</source>
+        <translation>Neschovávat</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Pronounce the word</source>
+        <translation>Vyslovit</translation>
+    </message>
+    <message>
+        <location filename="" line="1852400175"/>
+        <source>Pronounce using this command:</source>
+        <translation>Pro výslovnost použít tento příkaz:</translation>
+    </message>
+</context>
+</TS>
